bb51148a40fdf59f836cc4c4d5228b2cac8f8dd0
Ashwagandha-coder <max.cacz@yandex.ru>
2022-05-21 16:16:55 +0500

    override method run

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
+
+
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+    }
+}
+
+class TestThread extends Thread {
+
+
+    @Override
+    public void run() {
+
+
+
     }
 }
